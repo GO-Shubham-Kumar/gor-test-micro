3698a04065fdbfbecb47fc773b0db931b7964d4d
Shubham Kumar <unresolved.shubham@gmail.com>
2022-02-16 08:52:23 +0530

    Implemented Sync and Async Requests

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+import threading
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+import asyncio
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return ("Hello World!")
+@app.route("/", methods=['GET'])
+def index():
+    return {"data":"Sync Request Worked!"}
 
-api.add_resource(HelloWorld, "/")
+
+@app.route("/async", methods=["GET"])
+def async_req():
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(hello())
+    return jsonify({"result": result}), 200
+
+
+async def hello():
+    await asyncio.sleep(5)
+    return "Async Request Worked!"
 
 if __name__ == "__main__":
     app.run(debug=True)
