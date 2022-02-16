be7b8e9bac887da042b45c3d32b5f0ea4d7741f1
Shubham Kumar <unresolved.shubham@gmail.com>
2022-02-16 16:50:10 +0530

    Websocket chat implemented

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import threading
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_restful import Resource, Api
 import asyncio
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 api = Api(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route("/", methods=['GET'])
+@app.route("/sync", methods=['GET'])
 def index():
     return {"data":"Sync Request Worked!"}
 
@@ -18,10 +22,20 @@ def async_req():
     result = loop.run_until_complete(hello())
     return jsonify({"result": result}), 200
 
-
 async def hello():
     await asyncio.sleep(5)
     return "Async Request Worked!"
 
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+    send(message, broadcast=True)
+
+@app.route("/websocket", methods=["GET"])
+def websocket():
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
