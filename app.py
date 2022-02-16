e46dc8ba673c083fe5760a7483f52ca71085f2cd
Shubham Kumar <unresolved.shubham@gmail.com>
2022-02-16 23:58:22 +0530

    new

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import threading
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_restful import Resource, Api
 import asyncio
 from flask_socketio import SocketIO, send
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = 'secret!'
 api = Api(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', "POST"])
 @app.route("/sync", methods=['GET'])
 def index():
     now = datetime.now().strftime("%H:%M:%S")
@@ -20,10 +20,10 @@ def index():
 @app.route("/async", methods=["GET"])
 def async_req():
     receiving_time = datetime.now().strftime("%H:%M:%S")
-
+    delta = request.args.get('delta', default=5, type=int)
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(hello())
+    result = loop.run_until_complete(hello(delta))
 
     sending_time = datetime.now().strftime("%H:%M:%S")
 
@@ -33,8 +33,8 @@ def async_req():
         "sending_time": sending_time
         }), 200
 
-async def hello():
-    await asyncio.sleep(5)
+async def hello(wait_time=5):
+    await asyncio.sleep(wait_time)
     return "Async Request Worked!"
 
 
@@ -43,7 +43,7 @@ def handle_message(message):
     print('received message: ' + message)
     send(message, broadcast=True)
 
-@app.route("/websocket", methods=["GET"])
+@app.route("/websocket", methods=["GET", "POST"])
 def websocket():
     return render_template("index.html")
 
